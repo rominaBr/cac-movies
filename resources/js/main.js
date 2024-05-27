2557002aa7eec794cb6e12ff758267351ba312bf
rominabr <romina.brutti@gmail.com>
2024-05-26 22:54:02 -0300

    validaciones

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,7 +5,7 @@ const options = {
     method: 'GET', 
     headers: {
         accept: 'application/json', 
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Njc0ZjM3NDA1YmQxZWYyMjAwY2Y2YzEyNDI5NGM2ZCIsInN1YiI6IjY2NTNhNDBhZWZiMWIxMzE0MzMxNzk2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.paS50HXLF9Fbwp_CT49CXAq1IlJ1X4qEKm6jsFMHT4Y'
+        Authorization: `Bearer ${API_KEY}`
         
     }
 };
